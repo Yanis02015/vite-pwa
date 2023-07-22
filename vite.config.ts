98e28756aade3d2f325a310129445451786d0802
Yanis Oulhaci <yaniscrab@gmail.com>
2023-07-22 01:22:18 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Weather Ups",
+    short_name: "Weathe Ups",
+    description: "An app that can show weather forecast for your city.",
+    icons: [
+      {
+        src: "/vite.svg",
+        sizes: "192x192",
+        type: "image/svg",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
